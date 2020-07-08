7f07f431b96d003873ebf8bb5060ab59fd365cd9
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-07-08 15:31:16 +0530

    Add login and register get routes and views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
 	res.send('This is the index page');
 });
 
+router.get('/login', (req, res) => {
+	res.render('../views/login.ejs');
+});
+
+router.get('/register', (req, res) => {
+	res.render('../views/register.ejs')
+});
+
 module.exports = router;
